66d0eacc786fffcd28697e089bc12221d6fbad29
Giovanni Pellegrini <gianni.pele92@gmail.com>
2020-05-20 00:57:33 +0200

    fix3

--- examples/graphproppred/mol/gnn.py
+++ examples/graphproppred/mol/gnn.py
@@ -11,7 +11,7 @@ from torch_scatter import scatter_mean
 class GNN(torch.nn.Module):
 
     def __init__(self, num_tasks, num_layer = 5, emb_dim = 300, 
-                    gnn_type = 'gin', virtual_node = True, residual = False, drop_ratio = 0.5, JK = "last", graph_pooling = "mean", laf_fun='mean', laf_layers='false', device=device):
+                    gnn_type = 'gin', virtual_node = True, residual = False, drop_ratio = 0.5, JK = "last", graph_pooling = "mean", laf_fun='mean', laf_layers='false', device='cuda'):
         '''
             num_tasks (int): number of labels to be predicted
             virtual_node (bool): whether to add virtual node or not
